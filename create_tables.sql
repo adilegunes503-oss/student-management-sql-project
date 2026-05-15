373ceaf2628677a75a683939d23a8a073ff4efe5
adilegunes503-oss <adilegunes503@gmail.com>
2026-05-15 16:22:06 +0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,23 @@
-INSERT INTO Students VALUES
-('Ali','Can','Programming'),
-('Zeynep','Sahin','Programming'),
-('Merve','Aydin','Programming');
+CREATE TABLE Students(
+    student_id INT PRIMARY KEY IDENTITY(1,1),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    department VARCHAR(50)
+);
 
-INSERT INTO Instructors VALUES
-('Ahmet Yilmaz'),
-('Ayse Kaya');
+CREATE TABLE Instructors(
+    instructor_id INT PRIMARY KEY IDENTITY(1,1),
+    full_name VARCHAR(50)
+);
 
-INSERT INTO Courses VALUES
-('Database Systems'),
-('Programming');
+CREATE TABLE Courses(
+    course_id INT PRIMARY KEY IDENTITY(1,1),
+    course_name VARCHAR(50)
+);
+
+CREATE TABLE Enrollments(
+    enrollment_id INT PRIMARY KEY IDENTITY(1,1),
+    student_id INT,
+    course_id INT,
+    grade INT
+);
