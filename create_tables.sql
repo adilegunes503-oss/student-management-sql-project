8d777a00f5417a38a4c6f0aee8e1308de3b20e1d
adilegunes503-oss <adilegunes503@gmail.com>
2026-05-15 16:11:43 +0300

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,12 @@
+INSERT INTO Students VALUES
+('Ali','Can','Programming'),
+('Zeynep','Sahin','Programming'),
+('Merve','Aydin','Programming');
+
+INSERT INTO Instructors VALUES
+('Ahmet Yilmaz'),
+('Ayse Kaya');
+
+INSERT INTO Courses VALUES
+('Database Systems'),
+('Programming');
